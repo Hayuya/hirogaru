e83fc9efe15b60d67a53a8ea74b3e77487229d4e
hayuya <e3131901@foreign.kitakyu-u.ac.jp>
2025-10-06 00:21:51 +0900

    基本給消す

--- src/components/CompanyCard.tsx
+++ src/components/CompanyCard.tsx
@@ -197,7 +197,6 @@ export const CompanyCard: React.FC<CompanyCardProps> = ({ company, isRestricted,
                 {company.number_of_employees ? <DetailItem label="従業員数" value={`${company.number_of_employees}名`} /> : null}
                 {company.revenue && company.revenue !== 'N/A' && <DetailItem label="売上高" value={company.revenue} />}
                 {company.starting_salary_graduates && company.starting_salary_graduates !== 'N/A' && <DetailItem label="初任給" value={company.starting_salary_graduates} />}
-                {company.base_salary && company.base_salary !== 'N/A' && <DetailItem label="基本給" value={company.base_salary} />}
                 {isTruthy(company.fixed_overtime_system) && company.fixed_overtime_allowance && company.fixed_overtime_allowance !== 'N/A' && <DetailItem label="固定残業代" value={company.fixed_overtime_allowance} />}
                 {company.annual_holidays ? <DetailItem label="年間休日" value={`${company.annual_holidays}日`} /> : null}
                 {company.average_age ? <DetailItem label="平均年齢" value={`${company.average_age}歳`} /> : null}
